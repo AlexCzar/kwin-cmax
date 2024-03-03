dd4d6ce2ad4a1d8ddd1778e4cd65b690c5fd70a8
AlexCzar <AlexCzar@users.noreply.github.com>
2024-03-03 16:01:05 +0100

    implement the base functionality

---
+++ contents/code/main.js
@@ -0,0 +1,27 @@
+function almost_maximise(margin_v, margin_h) {
+    const client = workspace.activeClient;
+    if (client.moveable) {
+        const maxArea = workspace.clientArea(KWin.MaximizeArea, client);
+        const newWidth = maxArea.width - (margin_h || margin_v);
+        const newHeight = maxArea.height - margin_v;
+        client.geometry = {
+            x: maxArea.x + (maxArea.width - newWidth) / 2,
+            y: maxArea.y + (maxArea.height - newHeight) / 2,
+            width: newWidth,
+            height: newHeight
+        };
+    }
+}
+
+registerShortcut(
+    "CMax: almost maximise",
+    "Almost maximise the active window",
+    "Meta+Up",
+    () => { almost_maximise(100); }
+);
+registerShortcut(
+    "CMax: reasonable size",
+    "Set \"reasonable\" size to the active window",
+    "Meta+Shift+Up",
+    () => { almost_maximise(200, 600); }
+);
